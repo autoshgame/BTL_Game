d5f7134f51083bb55d3056ea786c3683d8771ad1
HieuhSSS <hieuhs@widogame.com>
2022-10-31 11:27:00 +0700

    Add project

---
+++ Assets/Resources/Scripts/GUI/MainMenu/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public void PlayTutorial() {
+        PlayMenuSound();
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void PlayLevel1() {
+        PlayMenuSound();
+        SceneManager.LoadScene("Level1");
+    }
+    
+    public void PlayLevel2() {
+        PlayMenuSound();
+        Debug.LogWarning("Scene for Level 2 does not exist");
+    }
+
+    public void PlayLevel3() {
+        PlayMenuSound();
+        Debug.LogWarning("Scene for Level 3 does not exist");
+    }
+
+    public void QuitGame() {
+        PlayMenuSound();
+        Application.Quit();
+    }
+
+    public void PlayMenuSound() {
+        SoundManager.PlaySound("MenuSound");
+    }
+}
---
+++ Assets/Resources/Scripts/PlayerAbilities/ActivateForceShield.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ActivateForceShield : MonoBehaviour {
+    private GameObject forceShield;
+    private CapsuleCollider2D bc2d;
+    private PlayerHealth playerHealth;
+    private bool wait;
+    
+    void Start() {
+        forceShield = Resources.Load<GameObject>("Prefabs/PlayerAbilities/ForceShield");
+        bc2d = GetComponent<CapsuleCollider2D>();
+        playerHealth = GetComponent<PlayerHealth>();
+        wait = true;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("Fire2") && wait && Time.timeScale != 0) {
+            StartCoroutine(playerInvinsible());
+            GameObject shieldInstance = Instantiate(forceShield, transform.position, transform.rotation);
+            shieldInstance.transform.parent = gameObject.transform;
+            Destroy(shieldInstance, 1f);
+        }
+    }
+
+    IEnumerator playerInvinsible() {
+        playerHealth.invinsible = true;
+        wait = false;
+        yield return new WaitForSeconds(1f);
+        playerHealth.invinsible = false;
+        yield return new WaitForSeconds(3f);
+        wait = true;
+    }
+}
